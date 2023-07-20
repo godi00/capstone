51c73dbd1a3499e5d3dcd05191f0baa44f5e3d47
godi00 <112918876+godi00@users.noreply.github.com>
2023-07-20 09:43:45 +0900

    Create AdminPage.js
    관리자 페이지

---
+++ src/components/page/admin/AdminPage.js
@@ -0,0 +1,16 @@
+/**
+ * ./src/components/page/admin/AdminPage.js
+ * 관리자 페이지
+ */
+
+const AdminPage = () => {
+    return(
+        <>
+            <div>
+                관리자 페이지입니다.
+            </div>
+        </>
+    );
+};
+
+export default AdminPage
