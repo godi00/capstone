b858824f14fe31267cacddbd663ea8ac206f3a48
godi00 <112918876+godi00@users.noreply.github.com>
2023-08-03 15:41:02 +0900

    header css
    파일 따로 빼내기 + 살짝 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,9 @@ import { Link } from 'react-router-dom';
 import { auth } from '../firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 
+// import css
+import "../style/Header.scss";
+
 // Header
 const Header = () => {
     const [userState, setUserState] = useState(false); // 로그인, 비로그인
@@ -30,32 +33,36 @@ const Header = () => {
 
     return (
             <div className='header'>
-                <div className='header-text-div'>
-                    <h3>파인드독<br/>빠른 실종 반려견 찾기</h3>
-                </div>
-    
-                <div className='header-logo-div'>
-                    <Link to="/">
-                        <img className='header-logo' src={process.env.PUBLIC_URL + 'images/logo_web.png'} />
-                    </Link>
+                <div className='header-left-div'>
+                    <div className='header-text-div'>
+                        <h3>파인드독<br/>빠른 실종 반려견 찾기</h3>
+                    </div>
+        
+                    <div className='header-logo-div'>
+                        <Link to="/">
+                            <img className='header-logo' src={process.env.PUBLIC_URL + 'images/logo_web.png'} />
+                        </Link>
+                    </div>
                 </div>
                 
-                <div className='header-admin-div'>
-                    {
-                        // 관리자 페이지 들어갈 수 있는 아이콘 삽입
-                        // userState에서 admin id 검사?
-                        (isAdmin)
-                        ? (<Link to="/admin"><img className='header-admin' src={process.env.PUBLIC_URL + 'images/support.png'} /></Link>)
-                        : (isAdmin)
-                    }
-                </div>
-    
-                <div className='header-profile-div'>
-                    {
-                        (userState)
-                        ? (<Link to="/mypage"><img className='header-profile' src={process.env.PUBLIC_URL + 'images/user.png'} /></Link>)
-                        : (<Link to="/login"><img className='header-profile' src={process.env.PUBLIC_URL + 'images/user.png'} /></Link>)
-                    }
+                <div className='header-right-div'>
+                    <div className='header-admin-div'>
+                        {
+                            // 관리자 페이지로 들어갈 수 있는 아이콘 삽입
+                            // userState에서 admin id 검사(isAdmin)
+                            (isAdmin)
+                            ? (<Link to="/admin"><img className='header-admin' src={process.env.PUBLIC_URL + 'images/support.png'} /></Link>)
+                            : (isAdmin)
+                        }
+                    </div>
+        
+                    <div className='header-profile-div'>
+                        {
+                            (userState)
+                            ? (<Link to="/mypage"><img className='header-profile' src={process.env.PUBLIC_URL + 'images/user.png'} /></Link>)
+                            : (<Link to="/login"><img className='header-profile' src={process.env.PUBLIC_URL + 'images/user.png'} /></Link>)
+                        }
+                    </div>
                 </div>
             </div>
         )
