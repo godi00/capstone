cb8be5e851f48ab68b753f4c8d983bcf6927ae5f
godi00 <112918876+godi00@users.noreply.github.com>
2023-08-06 16:31:47 +0900

    mainpage explain

--- src/components/page/main/MainPage.js
+++ src/components/page/main/MainPage.js
@@ -17,29 +17,30 @@ export const MainPage = () => {
                 <BannerCarousel/>
                 <br/><br/>
                 <div className="Bind">
-                    <div className="Video">
-                        <ReactPlayer className="video1" 
-                        url="https://www.youtube.com/watch?v=XMsDXtFWJ2U" 
-                        width="400px"
-                        height="225px"
-                        playing={false}
-                        muted={true}
-                        controls={true}
-                        />
-                        <br/><br/>
-                        <ReactPlayer className="video2" 
-                        url="https://www.youtube.com/watch?v=tIJjeCMB4Jo" 
-                        width="400px"
-                        height="225px"
-                        playing={false}
-                        muted={true}
-                        controls={true}
-                        />
+                    <div className="video-explain-div">
+                        <div className="Video">
+                            <ReactPlayer className="video1" 
+                            url="https://www.youtube.com/watch?v=XMsDXtFWJ2U" 
+                            width="400px"
+                            height="225px"
+                            playing={false}
+                            muted={true}
+                            controls={true}
+                            />
+                            <br/><br/>
+                            <ReactPlayer className="video2" 
+                            url="https://www.youtube.com/watch?v=tIJjeCMB4Jo" 
+                            width="400px"
+                            height="225px"
+                            playing={false}
+                            muted={true}
+                            controls={true}
+                            />
+                        </div>
+                        <Explain/>
                     </div>
-                    <Explain/>
+                    <Explain2/>
                 </div>
-                <br/><br/>
-                <Explain2/>
                 <br/><br/>
             </div>
         </>
