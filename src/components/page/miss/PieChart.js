d8493efa881c44e90198b964765bc277f61d7064
ysujin <91712467+ysujin@users.noreply.github.com>
2023-09-24 19:52:17 +0900

    //

--- src/components/page/miss/PieChart.js
+++ src/components/page/miss/PieChart.js
@@ -48,7 +48,7 @@ export const PieChart = (props) => {
 
     return (
         <>
-            <svg width={400} height={339} >
+            <svg width={400} height={344.9} >
 
                 {/* #93c58c | #69a65f | #376330 */}
 
